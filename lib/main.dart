646423d3b9b5e4ef9a25bb12872613c1f0ebec6f
Augusto Xavier <augustocbx@gmail.com>
2020-12-04 13:54:24 -0300

    Testing firebase on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     Firebase.initializeApp().whenComplete(() {
-      Firestore.instance
-          .collection('col')
-          .document('doc')
-          .setData({'texto': 'daniel'});
+      FirebaseFirestore.instance
+          .collection('mensagens')
+          .doc()
+          .set({'texto': 'daniel', 'read': false, 'from': 'Daniel'});
     });
     print('xxxx');
     return MaterialApp(
